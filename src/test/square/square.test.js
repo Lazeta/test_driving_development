8b22952d507b31b7a7c91e1e7ccfc13eb21748b9
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-05-28 20:07:28 +0300

    continue tested

--- src/test/square/square.test.js
+++ src/test/square/square.test.js
@@ -1,27 +1,29 @@
 import { square } from "./square";
 
 describe('square' ,() => {
-    let mockValue;
+    // let mockValue;
     // перед каждым
-    beforeEach(() => {
-        
-    })
-    beforeAll(() => {
-
-    })
+    // beforeEach(() => {})
+    // beforeAll(() => {})
 
     test('Correct value', () => {
         expect(square(2)).toBe(4);
         expect(square(2)).toBeLessThan(5);
         expect(square(2)).toBeGreaterThan(3);
-        // expect(square(2)).not.toBeGreaterThan();
-        // expect(square(2)).toBeCalledTimes();
+        expect(square(2)).not.toBeGreaterThan(4);
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2)
+        expect(spyMathPow).toBeCalledTimes(1)
+    })
+
+    test('Correct value 2', () => {
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2)
+        expect(spyMathPow).toBeCalledTimes(0)
     })
 
     afterEach(() => {
-        // delete property
+        jest.clearAllMocks();
     })
-    beforeEach(() => {
-
-    })
+    // afterAll(() => {  })
 })
